a75875e9aaaae575f37b613a15f4ab9b37b7817a
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-25 16:16:00 -0600

    Create Poro

---
+++ app/poros/photo.rb
@@ -0,0 +1,21 @@
+class Photo
+
+  attr_reader :title,
+              :id,
+              :url,
+              :location,
+              :photographer,
+              :profile,
+              :source
+
+  def initialize(data)
+    @id = "null"
+    @title = data[:title]
+    @url = data[:url]
+    @location = data[:location]
+    @photographer = data[:photographer]
+    @profile = data[:profile]
+    @source = data[:source]
+  end
+
+end
