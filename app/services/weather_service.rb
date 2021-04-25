f6e5629505c99de922e0bb4aca5d4d1ff9919d7c
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-24 18:01:04 -0600

    Set up service

---
+++ app/services/weather_service.rb
@@ -0,0 +1,24 @@
+class WeatherService
+  def self.current_forecast(location)
+    location = GeocodingFacade.latitude_longitude(location)
+    response = conn.get("data/2.5/onecall") do |request|
+      request.params['lat'] = location.latitude
+      request.params['lon'] = location.longitude
+      request.params['exclude'] =  "minutely"
+      request.params['units'] =  "imperial"
+    end
+    WeatherService.parser(response.body)
+  end
+
+  private
+
+  def self.parser(body)
+    JSON.parse(body, symbolize_names: true)
+  end
+
+  def self.conn
+    Faraday.new('https://api.openweathermap.org/') do |request|
+      request.params['appid'] = ENV['weather_key']
+    end
+  end
+end
