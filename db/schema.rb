aef3ab57c1ccef0a43d2ebf2d597b55d9ac1ebdf
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-25 17:34:58 -0600

    Add user model, spec, migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_04_25_233101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+  end
 
 end
