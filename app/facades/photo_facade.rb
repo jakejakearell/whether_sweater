1e147d3c342f7589cfa4f5ad3316652858c5a9f1
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-25 16:16:29 -0600

    Add methods to facade

---
+++ app/facades/photo_facade.rb
@@ -0,0 +1,27 @@
+class PhotoFacade
+
+  def initialize(location)
+    @location = location
+    @service_call = PhotoService.photo_location_lat_lon_search(@location)
+  end
+
+  def photo_id
+    @service_call[:photos][:photo].first[:id]
+  end
+
+  def parsed_down_service_call
+    photo_service = PhotoService.photo_lookup(photo_id)
+    {
+      url: photo_service[:photo][:urls][:url].first[:_content],
+      location: @location,
+      source: "flickr.com",
+      photographer: photo_service[:photo][:owner][:username],
+      profile: "https://www.flickr.com/photos/#{photo_service[:photo][:owner][:nsid]}",
+      title: photo_service[:photo][:title][:_content]
+    }
+  end
+
+  def photo_object
+    Photo.new(parsed_down_service_call)
+  end
+end
