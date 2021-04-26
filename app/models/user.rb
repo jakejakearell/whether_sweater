85345ca0a2ad264bcea4a2bf9514c384bae7afa6
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-25 19:20:12 -0600

    Change attribute names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
 
   has_secure_password
