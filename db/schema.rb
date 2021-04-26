52c7c7e8dae8d6d7540215cd029a58ab58388d34
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-25 19:20:52 -0600

    DB normalization

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_25_233101) do
+ActiveRecord::Schema.define(version: 2021_04_26_001757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "password_digest"
+    t.string "email"
+    t.string "api_key"
   end
 
 end
