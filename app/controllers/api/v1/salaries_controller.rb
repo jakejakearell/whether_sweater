974a25470065063720e5e2f4e91c65d655f2b741
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-26 11:21:10 -0600

    Clean up code

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,60 +1,12 @@
 class Api::V1::SalariesController < ApplicationController
-  include ActionView::Helpers::NumberHelper
-
   def index
     salaries_object = SalariesFacade.new(destination).salaries_object
     render json: SalariesSerializer.new(salaries_object)
   end
 
   private
-  #
-  # def salaries
-  #   Faraday.get("https://api.teleport.org/api/urban_areas/slug:#{destination}/salaries/")
-  # end
 
   def destination
     params[:destination]
   end
-  #
-  # def conn
-  #   Faraday.new('https://api.teleport.org/api')
-  # end
-  #
-  # def json_parser(body)
-  #   JSON.parse(body, symbolize_names: true)
-  # end
-  #
-  # def parse_salaries_api_call
-  #   collection = []
-  #   json_parser(salaries.body)[:salaries].map do |salary|
-  #     if targeted_jobs.include?(salary[:job][:title])
-  #       collection << salary
-  #     end
-  #   end
-  #   collection
-  # end
-  #
-  # def targeted_jobs
-  #   ["Data Analyst","Data Scientist","Mobile Developer","QA Engineer","Software Engineer","Systems Administrator","Web Developer"]
-  # end
-  #
-  # def format_salaries_information
-  #   parse_salaries_api_call.reduce([]) do |memo, salary|
-  #     info = {
-  #       title: salary[:job][:title],
-  #       min: number_to_currency(salary[:salary_percentiles][:percentile_25]),
-  #       max: number_to_currency(salary[:salary_percentiles][:percentile_75])
-  #     }
-  #     memo << info
-  #     memo
-  #   end
-  # end
-
-  # def weather_info
-  #   WeatherFacade.new(destination).current_weather_salaries
-  # end
-  #
-  # def salaries_object
-  #   Salaries.new(destination, format_salaries_information, weather_info)
-  # end
 end
