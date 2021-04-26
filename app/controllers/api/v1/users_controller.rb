ed0d19473dbd245fc72c79d6bf181d7e3e177305
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-25 19:19:34 -0600

    Add basic user creation code

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    require "pry"; binding.pry
+    user = User.new(user_params)
+    user.save
+    render json:UserSerializer.new(user), status: 201
   end
-end 
+
+
+  private
+  def user_params
+    {email: params["item"]["email"],password: params["item"]["password"], api_key:SecureRandom.hex }
+  end
+end
