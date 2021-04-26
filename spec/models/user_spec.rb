95c910e2da9d410c6345bd9b559f483d47071eef
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-25 19:18:52 -0600

    Change attribute names

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe User, type: :model do
   describe "validations" do
-    it {should validate_presence_of(:username)}
-    it {should validate_uniqueness_of(:username)}
+    it {should validate_presence_of(:email)}
+    it {should validate_uniqueness_of(:email)}
     it {should validate_presence_of(:password)}
   end
 end
