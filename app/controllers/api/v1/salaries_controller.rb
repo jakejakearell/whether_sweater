aa527bdec42f9aabb2d4c553f14f35677a6133b3
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-26 09:31:29 -0600

    Start assert data structure of call

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,6 +1,36 @@
 class Api::V1::SalariesController < ApplicationController
 
   def index
-
+    test = json_parser(salaries.body)
+    parse_salaries_api_call
+    require "pry"; binding.pry
   end
-end 
+
+  private
+
+  def salaries
+    Faraday.get("https://api.teleport.org/api/urban_areas/slug:#{destination}/salaries/")
+  end
+
+  def destination
+    params[:destination]
+  end
+
+  def conn
+    Faraday.new('https://api.teleport.org/api')
+  end
+
+  def json_parser(body)
+    JSON.parse(body, symbolize_names: true)
+  end
+
+  def parse_salaries_api_call
+    json_parser(salaries.body)[:salaries].each do |salary|
+      require "pry"; binding.pry
+    end
+  end
+
+  def targeted_jobs
+    ["Data Analyst","Data Scientist","Mobile Developer","QA Engineer","Software Engineer","Systems Administrator","Web Developer"]
+  end 
+end
--- spec/requests/api/v1/salaries_request_spec.rb
+++ spec/requests/api/v1/salaries_request_spec.rb
@@ -8,7 +8,23 @@ describe "Salaries endpoint" do
 
       expect(response).to be_successful
 
-      # salaires = JSON.parse(response.body, symbolize_names: true)
+      salaries = JSON.parse(response.body, symbolize_names: true)
+
+      expect(salaries).to be_a(Hash)
+      expect(salaries.count).to eq(1)
+      expect(salaries).to have_key(:data)
+      expect(salaries[:data]).to be_a(Hash)
+      expect(salaries[:data].count).to eq(3)
+      expect(salaries[:data]).to have_key(:id)
+      expect(salaries[:data][:id]).to eq("null")
+      expect(salaries[:data]).to have_key(:type)
+      expect(salaries[:data][:type]).to eq('salaries')
+      expect(salaries[:data]).to have_key(:attributes)
+      expect(salaries[:data][:attributes].count).to eq(3)
+      expect(salaries[:data][:attributes]).to have_key(:destination)
+      expect(salaries[:data][:attributes][:destination].count).to eq(1)
+      expect(salaries[:data][:attributes][:destination]).to eq('denver')
+
     end
   end
 end
