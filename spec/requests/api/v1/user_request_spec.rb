fdcfa785ed132c8a62cf79d10fc9a2fe68be35af
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-25 19:19:14 -0600

    Write happy path test

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -3,14 +3,36 @@ require 'rails_helper'
 describe "User Creation" do
   describe 'Happy Paths' do
     it "can create a user" do
-      user_params = ({
+      user_params = {
                  email: 'jake@ihopethiswork.org',
                  password: 'h1tech',
                  password_confitmation: 'h1tech'
-               })
-      headers = {"CONTENT_TYPE" => "application/json"}
+               }
+
+      headers = {"CONTENT_TYPE" => "application/json", 'ACCEPT' => 'application/json' }
 
       post "/api/v1/users", headers: headers, params: JSON.generate(item: user_params)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+
+      user_json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(user_json).to be_a(Hash)
+      expect(user_json.keys.count).to eq(1)
+      expect(user_json).to have_key(:data)
+      expect(user_json[:data].keys.count).to eq(3)
+      expect(user_json[:data]).to have_key(:id)
+      expect(user_json[:data][:id]).to be_a(String)
+      expect(user_json[:data]).to have_key(:type)
+      expect(user_json[:data][:type]).to eq("user")
+      expect(user_json[:data]).to have_key(:attributes)
+      expect(user_json[:data][:attributes].count).to eq(2)
+      expect(user_json[:data][:attributes]).to have_key(:api_key)
+      expect(user_json[:data][:attributes][:api_key]).to be_a(String)
+      expect(user_json[:data][:attributes]).to have_key(:email)
+      expect(user_json[:data][:attributes][:email]).to eq(user_params[:email])
+      expect(user_json[:data][:attributes][:email]).to be_a(String)
     end
   end
 end
