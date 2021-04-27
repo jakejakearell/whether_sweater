19e17990e855824ebb898611191ce76973406728
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-27 11:34:28 -0600

    Add happy and sad paths for roadtrip service

--- spec/services/geocoding_service_spec.rb
+++ spec/services/geocoding_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Geocoding Service' do
   describe 'Happy Paths' do
 
-    it 'will establish a connection to the geocoding service' do
+    it 'will establish a connection to the geocoding service for a location' do
       VCR.use_cassette('geocoding_service_test') do
         service = GeocodingService.address_cordinates('Denver,CO')
         expect(service).to be_a(Hash)
@@ -16,6 +16,23 @@ RSpec.describe 'Geocoding Service' do
         expect(service[:results].first[:locations].first[:latLng]).to have_key(:lng)
         expect(service[:results].first[:locations].first[:latLng][:lat]).to eq(39.738453)
         expect(service[:results].first[:locations].first[:latLng][:lng]).to eq((-104.984853))
+      end
+    end
+
+    it 'will establish a connection to the geocoding service for a roadtrip' do
+      VCR.use_cassette('geocoding_road_trip_service') do
+        destination = {"origin"=>"Denver,CO", "destination"=>"Pueblo,CO"}
+        service = GeocodingService.road_trip(destination)
+        expect(service).to be_a(Hash)
+        expect(service).to have_key(:route)
+        expect(service[:route]).to be_a(Hash)
+        expect(service[:route]).to have_key(:formattedTime)
+        expect(service[:route][:formattedTime]).to be_a(String)
+        expect(service[:route]).to have_key(:time)
+        expect(service[:route][:time]).to be_a(Integer)
+        expect(service[:route]).to have_key(:routeError)
+        expect(service[:route][:routeError]).to have_key(:errorCode)
+        expect(service[:route][:routeError][:errorCode]).to eq(-400)
       end
     end
   end
@@ -36,5 +53,41 @@ RSpec.describe 'Geocoding Service' do
         expect(service[:results].first[:locations].first[:latLng][:lng]).to be_a(Float)
       end
     end
+
+    it 'will will have an error for an impossible route' do
+      VCR.use_cassette('bad_location_geocoding_road_trip_service') do
+        destination = {"origin"=>"New York, NY", "destination"=>"London,UK"}
+
+        service = GeocodingService.road_trip(destination)
+        expect(service).to be_a(Hash)
+        expect(service).to have_key(:info)
+        expect(service[:info]).to have_key(:messages)
+        expect(service[:info][:messages]).to be_a(Array)
+        expect(service[:info][:messages].first).to eq("We are unable to route with the given locations.")
+        expect(service).to have_key(:route)
+        expect(service[:route]).to be_a(Hash)
+        expect(service[:route]).to have_key(:routeError)
+        expect(service[:route][:routeError]).to have_key(:errorCode)
+        expect(service[:route][:routeError][:errorCode]).to eq(2)
+      end
+    end
+
+    it 'will will have an error for an one destination' do
+      VCR.use_cassette('one_location_geocoding_road_trip_service') do
+        destination = {"origin"=>"New York, NY", "destination"=>""}
+        service = GeocodingService.road_trip(destination)
+        expect(service).to be_a(Hash)
+        expect(service).to have_key(:info)
+        expect(service[:info]).to have_key(:messages)
+        expect(service[:info][:messages]).to be_a(Array)
+        expect(service[:info][:messages].first).to eq("At least two locations must be provided.")
+        expect(service).to have_key(:route)
+        expect(service[:route]).to be_a(Hash)
+        expect(service[:route]).to have_key(:routeError)
+        expect(service[:route][:routeError]).to have_key(:errorCode)
+        expect(service[:route][:routeError][:errorCode]).to eq(211)
+      end
+    end
+
   end
 end
