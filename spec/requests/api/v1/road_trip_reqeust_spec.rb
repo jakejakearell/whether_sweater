a9179508c678ceefa8f45f9d4cdf579d0b670b7e
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-27 17:47:54 -0600

    Add sad path testing

--- spec/requests/api/v1/road_trip_reqeust_spec.rb
+++ spec/requests/api/v1/road_trip_reqeust_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 describe "Roadtrip Endpoint" do
-  describe 'Happy Paths' do
-    before :each do
-      body = {
-                 email: 'jake@ihopethiswork.org',
-                 password: 'h1tech',
-                 password_confirmation: 'h1tech'
-               }
-      headers = {"CONTENT_TYPE" => "application/json", 'ACCEPT' => 'application/json' }
-      post "/api/v1/users", headers: headers, params: body, as: :json
-      @user = User.first
-    end
+  before :each do
+    body = {
+      email: 'jake@ihopethiswork.org',
+      password: 'h1tech',
+      password_confirmation: 'h1tech'
+    }
+    headers = {"CONTENT_TYPE" => "application/json", 'ACCEPT' => 'application/json' }
+    post "/api/v1/users", headers: headers, params: body, as: :json
+    @user = User.first
+  end
 
+  describe 'Happy Paths' do
     it "will return JSON with a proper request create a user" do
       body =  {
                 "origin": "Denver,CO",
@@ -68,11 +68,88 @@ describe "Roadtrip Endpoint" do
                 }
 
         headers = {"CONTENT_TYPE" => "application/json", 'ACCEPT' => 'application/json' }
-        post "/api/v1/road_trip", headers: headers, params: body, as: :json
-        expect(response.status).to eq(401)
+        VCR.use_cassette("bad_login") do
+          post "/api/v1/road_trip", headers: headers, params: body, as: :json
+          expect(response.status).to eq(401)
 
-        user_json = JSON.parse(response.body, symbolize_names: true)
-        expect(user_json[:error]).to eq("Unauthorized")
+          no_api = JSON.parse(response.body, symbolize_names: true)
+          expect(no_api[:error]).to eq("Unauthorized")
+        end
+      end
+    end
+
+    describe 'destination will cannot be reached by car' do
+      it "returns a JSON with impossible in several fields" do
+        body =  {
+                  "origin": "Denver,CO",
+                  "destination": "London,UK",
+                  "api_key": "#{@user.api_key}"
+                }
+
+        headers = {"CONTENT_TYPE" => "application/json", 'ACCEPT' => 'application/json' }
+        VCR.use_cassette("road_trip_not_passbable") do
+          post "/api/v1/road_trip", headers: headers, params: body, as: :json
+          expect(response.status).to eq(200)
+
+          road_trip_response = JSON.parse(response.body, symbolize_names: true)
+
+          expect(road_trip_response).to be_a(Hash)
+          expect(road_trip_response).to have_key(:data)
+          expect(road_trip_response[:data]).to be_a(Hash)
+          expect(road_trip_response[:data]).to have_key(:id)
+          expect(road_trip_response[:data][:id]).to eq("null")
+          expect(road_trip_response[:data]).to have_key(:type)
+          expect(road_trip_response[:data][:type]).to eq('road_trip')
+          expect(road_trip_response[:data]).to have_key(:attributes)
+          expect(road_trip_response[:data][:attributes]).to be_a(Hash)
+          expect(road_trip_response[:data][:attributes].count).to eq(4)
+          expect(road_trip_response[:data][:attributes]).to have_key(:start_city)
+          expect(road_trip_response[:data][:attributes][:start_city]).to eq("Denver,CO")
+          expect(road_trip_response[:data][:attributes]).to have_key(:end_city)
+          expect(road_trip_response[:data][:attributes][:end_city]).to eq("London,UK")
+          expect(road_trip_response[:data][:attributes]).to have_key(:travel_time)
+          expect(road_trip_response[:data][:attributes][:travel_time]).to eq("impossible")
+          expect(road_trip_response[:data][:attributes]).to have_key(:weather_at_eta)
+          expect(road_trip_response[:data][:attributes][:weather_at_eta]).to be_a(Hash)
+          expect(road_trip_response[:data][:attributes][:weather_at_eta].count).to eq(2)
+          expect(road_trip_response[:data][:attributes][:weather_at_eta]).to have_key(:temperature)
+          expect(road_trip_response[:data][:attributes][:weather_at_eta][:temperature]).to eq("impossible")
+          expect(road_trip_response[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
+          expect(road_trip_response[:data][:attributes][:weather_at_eta][:conditions]).to eq("impossible")
+        end
+      end
+    end
+
+    describe 'bad params will throw return a message' do
+      it "returns error message" do
+        body =  {
+                  "s": "23,12",
+                  "sdasd": "123",
+                  "api_key": "#{@user.api_key}"
+                }
+
+        headers = {"CONTENT_TYPE" => "application/json", 'ACCEPT' => 'application/json' }
+        VCR.use_cassette("road_trip_bad_body") do
+          post "/api/v1/road_trip", headers: headers, params: body, as: :json
+          expect(response.status).to eq(401)
+
+          road_trip_response = JSON.parse(response.body, symbolize_names: true)
+
+          expect(road_trip_response[:error]).to eq("Bad Params")
+        end
+      end
+
+      it "returns error message" do
+        body =  nil
+        headers = {"CONTENT_TYPE" => "application/json", 'ACCEPT' => 'application/json' }
+        VCR.use_cassette("no body") do
+          post "/api/v1/road_trip", headers: headers, params: body, as: :json
+          expect(response.status).to eq(401)
+
+          road_trip_response = JSON.parse(response.body, symbolize_names: true)
+
+          expect(road_trip_response[:error]).to eq("Missing Params")
+        end
       end
     end
   end
