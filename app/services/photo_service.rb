ccc650dbf8fd9e243ffdc3ae8d6e59897f0eab6b
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-27 21:22:29 -0600

    Refactor services

--- app/services/photo_service.rb
+++ app/services/photo_service.rb
@@ -1,12 +1,14 @@
 class PhotoService < ApplicationService
 
   def self.photo_location_lat_lon_search(location)
-    location = GeocodingFacade.new(location).latitude_longitude
+    location = GeocodingFacade.new(location).check_service_call_status
     response = conn.get("/services/rest") do |request|
       request.params['method'] =  "flickr.photos.search"
       request.params['lat'] = location.latitude
       request.params['lon'] = location.longitude
+      # accuracy is a scale 1-16 of how close a photo is to lat/long with 1 being far
       request.params['accuracy'] = 11
+      # 2 is a flag for a photo to be outside
       request.params['geo_context'] =  2
     end
     PhotoService.parser(response.body)
