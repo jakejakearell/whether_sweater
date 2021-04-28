423051f0132bf586cdd9292cc5fffc254f2fb139
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-28 05:54:15 -0600

    Add sad path

--- spec/poros/road_trip_spec.rb
+++ spec/poros/road_trip_spec.rb
@@ -21,4 +21,24 @@ RSpec.describe RoadTrip do
       expect(@result.weather_at_eta[:conditions]).to be_a(String)
     end
   end
+
+  describe 'sad path' do
+    before :each do
+      VCR.use_cassette("road_trip_poro_sad") do
+        @result_2 = RoadTrip.new(nil, nil)
+      end
+    end
+
+    it 'has attributes of current_weather, daily_weather, hourly_weather and is a RoadTrip object' do
+      expect(@result_2).to be_a(RoadTrip)
+      expect(@result_2.id).to eq("null")
+      expect(@result_2.end_city).to eq("none")
+      expect(@result_2.start_city).to eq("none")
+      expect(@result_2.travel_time).to eq(nil)
+      expect(@result_2.weather_at_eta).to be_a(Hash)
+      expect(@result_2.weather_at_eta.count).to eq(2)
+      expect(@result_2.weather_at_eta[:temperature]).to eq("impossible")
+      expect(@result_2.weather_at_eta[:conditions]).to eq("impossible")
+    end
+  end
 end
