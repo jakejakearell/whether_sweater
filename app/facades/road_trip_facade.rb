d0c00e0a829609ba3a427fd2d840dd7645103fc8
Jacob Arellano <jmm.arellano@gmail.com>
2021-04-28 06:51:54 -0600

    Finish facade

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -6,26 +6,14 @@ class RoadTripFacade
     @data = GeocodingService.road_trip(destination)
   end
 
-  def destination_location
-    {
-      :longitude => @data[:route][:boundingBox][:lr][:lng],
-      :latitude => @data[:route][:boundingBox][:lr][:lat]
-    }
-  end
-
-  def route_time
-    @data[:route][:formattedTime]
-  end
-
-  def forty_eight_hours_of_forecast
-    WeatherService.hourly(destination_location)[:hourly]
+  def eta
+    @data[:route][:realTime] + (Time.now).to_i
   end
 
   def choose_time
-    eta = @data[:route][:realTime] + (Time.now).to_i
-
+    trip_eta = eta
     forty_eight_hours_of_forecast.min_by do |hour|
-      (hour[:dt] - eta).abs
+      (hour[:dt] - trip_eta).abs
     end
   end
 
@@ -37,5 +25,20 @@ class RoadTripFacade
     else
       RoadTrip.new(choose_time, route_time, destination )
     end
+  end
+  
+  def forty_eight_hours_of_forecast
+    WeatherService.hourly(destination_location)[:hourly]
+  end
+
+  def destination_location
+    {
+      :longitude => @data[:route][:boundingBox][:lr][:lng],
+      :latitude => @data[:route][:boundingBox][:lr][:lat]
+    }
+  end
+
+  def route_time
+    @data[:route][:formattedTime]
   end
 end
